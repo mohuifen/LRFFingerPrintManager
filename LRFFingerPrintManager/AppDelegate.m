427d1c43e8896266075cd71d0d0b61b0bef95c40
LRF <lvruifei@huobi.com>
2016-02-04 11:21:22 +0800

    添加 buddybuild

--- LRFFingerPrintManager/AppDelegate.m
+++ LRFFingerPrintManager/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import <BuddyBuildSDK/BuddyBuildSDK.h>
 @interface AppDelegate ()
 
 @end
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [BuddyBuildSDK setup];
     return YES;
 }
 
